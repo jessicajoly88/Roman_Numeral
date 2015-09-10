d591ba123d77ed278ebaa46e1b76fde716a286ac
Jessica Joly & Lisa Folliard <student@epicodus.com>
2015-09-10 14:35:41 -0700

    Second spec pass and JS code works to generate roman numeral symbols that represent entered number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var romanNumeral = function(number) {
-
-
   var symbols = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000};
+  var array = [];
 
   for(var key in symbols){
     if (symbols[key] === number){
@@ -9,4 +8,33 @@ var romanNumeral = function(number) {
     };
   };
 
+  while (number >= 1000) {
+    array.push("M");
+    number -= 1000;
+  }
+  while (number >= 500) {
+    array.push("D");
+    number -= 500;
+  }
+  while (number >= 100) {
+    array.push("C");
+    number -= 100;
+  }
+  while (number >= 50) {
+    array.push("L");
+    number -= 50;
+  }
+  while (number >= 10) {
+    array.push("X");
+    number -= 10;
+  }
+  while (number >= 5) {
+    array.push("V");
+    number -= 5;
+  }
+  while (number >= 1) {
+    array.push("I");
+    number -= 1;
+  }
+  return array.join('');
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("romanNumeral", function() {
   it('inputs roman numeral value and returns equivalent roman numeral', function() {
     expect(romanNumeral(5)).to.equal("V");
   });
+
+  it('input value and returns up to three roman numeral symbols', function() {
+    expect(romanNumeral(6)).to.equal("VI");
+  });
 });
